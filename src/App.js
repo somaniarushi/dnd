d07a5fd94ce3f4f4ea644499bf5b9d9748b5aff9
Arushi Somani <arushisomani@berkeley.edu>
2021-06-01 18:15:05 -0700

    adjusting font size

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   else {
     return (
       <div>
-        <ReactJson src={charBuilder(selection)} name="Character Sheet" indentWidth={8} displayDataTypes={false} displayObjectSize={false} collapseStringsAfterLength={50} style={{fontSize: "1.5em"}}/>
+        <ReactJson src={charBuilder(selection)} name="Character Sheet" indentWidth={8} displayDataTypes={false} displayObjectSize={false} collapseStringsAfterLength={50} style={{fontSize: "1.2em"}}/>
     </div>
     );
   }
